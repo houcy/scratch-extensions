6e54c2213128529b92a76b660d35c63357e9a79b
Liam (Aqua) <liam4@users.noreply.github.com>
2015-05-09 18:12:50 -0300

    Update firstextension.js
    + first_block
    + random

--- firstextension/firstextension.js
+++ firstextension/firstextension.js
@@ -1,4 +1,22 @@
 (function(ext) {
-  // Cleanup function when the extension is unloaded
   ext._shutdown = function() {};
+  
+  ext._getStatus = function() {
+    return {status: 2, msg: 'Ready'};
+  };
+  
+  ext.first_block = function() {
+    alert("Hello, world!");
+  };
+  
+  ext.random = function() {
+    return Math.random();
+  };
+  
+  var descriptor = {
+    blocks: [
+      [' ', 'first block', 'first_block'],
+      ['r', 'random 0-1', 'random']
+    ]
+  };
 })({});
