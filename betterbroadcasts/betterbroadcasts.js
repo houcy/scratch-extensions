ebd3ebe0262dca2396aa82ec2f0dd3360b47fb35
Liam (Aqua) <liam4@users.noreply.github.com>
2015-05-10 11:51:42 -0300

    Update betterbroadcasts.js

--- betterbroadcasts/betterbroadcasts.js
+++ betterbroadcasts/betterbroadcasts.js
@@ -17,13 +17,18 @@ betterBroadcasts = {
     return {status: 2, msg: 'Ready'};
   };
   
+  ext.broadcastGo = function(bcname) {betterBroadcasats.broadcastNow[bcname] = true};
+  ext.broadcastHat = function(bcname) {return betterBroadcasts.broadcastNow[bcname] === true};
+  
   var descriptor = {
     blocks: [
       // Type, spec, function (in quotes), default values.
+      ['h', 'when I receive bbc %s', 'better broadcast 1'],
+      [' ', 'better broadcast %s', 'better broadcast 1']
     ],
     menus: {
       // Name: [contents...]
-    }
+    },
     url: "https://liam4.github.io/scratch-extensions/index.php#betterbroadcasts",
   };
   
